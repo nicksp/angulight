e7e761598055b7c5e2cd4c676f8ee37d64683b99
Nick Plekhanov <nickatgit@yandex.com>
2016-04-23 16:25:08 +0200

    Implement separated watchers on child scopes.

--- src/scope.js
+++ src/scope.js
@@ -244,6 +244,7 @@ Scope.prototype.$new = function () {
   var ChildScope = function () {};
   ChildScope.prototype = this;
   var child = new ChildScope();
+  child.$$watchers = [];
   return child;
 };
 
--- test/scopeSpec.js
+++ test/scopeSpec.js
@@ -980,6 +980,22 @@ describe('Scope', function () {
       expect(parent.user.name).toBe('Ben');
     });
 
+    it('does not digest its parent(s)', function () {
+      var parent = new Scope();
+      var child = parent.$new();
+
+      parent.aValue = 'abc';
+      parent.$watch(
+        function (scope) { return scope.aValue; },
+        function (newValue, oldValue, scope) {
+          scope.aValueWas = newValue;
+        }
+      );
+
+      child.$digest();
+      expect(child.aValueWas).toBeUndefined();
+    });
+
     it('', function () {
 
     });
